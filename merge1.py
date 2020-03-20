f51216dab41f772dd4e71979b6d92f4a431f034e
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-03-20 13:36:14 +0200

    Add files via upload

---
+++ merge1.py
@@ -0,0 +1,18 @@
+#l1 = [1,4,5,8]
+#l2 = [2,6,10]
+
+
+def merge(l1,l2):
+    l = l1 + l2
+    n = len(l)
+    i = 0
+    while i < n - 1:
+        j = 0
+        while j < n - 1 - i: # < I'm sad(((
+            if l[j] > l[j + 1]:
+                l[j], l[j + 1] = l[j + 1], l[j]
+            j += 1
+        i += 1
+    return l
+
+print(merge([1,4,5,8],[2,6,10]))

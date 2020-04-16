79bcc09468c09d86dfe63f9436838a2c5128cda4
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-04-16 13:48:07 +0300

    add polindrom 4.py

--- polindrom 4.py
+++ polindrom 4.py
@@ -3,13 +3,11 @@
 
 def word_poly(word):
     s = word.replace(" ", "")
-
     l = len(s)
 
     for i in range(l//2):
         if s[i] != s[-1-i]:
             return False
-
     return True
 
 print(word_poly('mr owl ate my metal worm'))

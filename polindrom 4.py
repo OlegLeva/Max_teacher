105c021964e030ebe4711974ec5c4efc54db300e
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-04-16 18:34:30 +0300

    l

--- polindrom 4.py
+++ polindrom 4.py
@@ -12,19 +12,3 @@ def word_poly(word):
 
 print(word_poly('mr owl ate my metal worm'))
 
-
-#check if the string is a polindrome
-
-import re
-
-def word_poly(word):
-    pat = r'\,|\s|\:|\.|\-|\_'
-    s = re.sub(pat, '')
-    l = len(s)
-
-    for i in range(l//2):
-        if s[i] != s[-1-i]:
-            return False
-    return True
-
-print(word_poly('mr owl ate my metal worm'))

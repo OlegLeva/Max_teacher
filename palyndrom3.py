7c39a11704877dfe474c5ca90374a24b7e64f6c5
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-04-17 11:41:17 +0300

    УРРРААААА!!!

--- palyndrom3.py
+++ palyndrom3.py
@@ -4,12 +4,10 @@ def palindrom(s):
     i = 0
     j = len(s) - 1
     while i < j:
-        #if s[i] == ' ':  # ????????
-            #continue
+        if s[i] == ' ':  i += 1
         while not s[i] and i < j:
             i += 1
-        #if s[j] == ' ':  # ?????????
-            #continue
+        if s[j] == ' ':  j -= 1
         while not s[j] and i < j:
             j -= 1
         if s[i] != s[j]:
@@ -17,5 +15,5 @@ def palindrom(s):
         i += 1
         j -= 1
     return True
-print(palindrom('abcdcba'))
+print(palindrom('mr owl ate my metal worm'))
 

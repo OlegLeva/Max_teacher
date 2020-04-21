1af88e621b74acb1c2bc8cf2bff88f624cddc366
OlegLeva <60513057+OlegLeva@users.noreply.github.com>
2020-04-21 23:11:05 +0300

    no duplicates update

--- sort_list2.py
+++ sort_list2.py
@@ -1,14 +1,30 @@
+def merge(l1, l2):
 
-def merge(l1):
     res = []
-    res.append(l1[0])
-    i = 0
-    while i < len(l1)-1:
-        if l1[i] < l1[i+1]:
-            res.append(l1[i+1])
-        if l1[i] == l1[i+1]:
-            pass
-        i += 1
-    return res
+    res1 = []
 
-print(merge([-1,-1, 0, 1, 2, 2, 3, 3, 5, 7, 8, 9]))
+    i, j, r = 0, 0, 0
+    while i < len(l1) and j < len(l2):
+        if l1[i] <= l2[j]:
+            res.append(l1[i])
+            i += 1
+        else:
+            res.append(l2[j])
+            j += 1
+    # copy rest
+    if i < len(l1):
+        res.extend(l1[i:])
+    if j < len(l2):
+        res.extend(l2[j:])
+
+    res1.append(res[0])
+    while r < len(res) - 1:
+        if res[r] < res[r + 1]:
+            res1.append(res[r + 1])
+        if res[r] == res[r + 1]:
+            pass
+        r += 1
+    return res1
+
+
+print(merge([1, 2, 2, 3, 3, 3, 7, 9], [-1, -1, 0, 5, 8]))
